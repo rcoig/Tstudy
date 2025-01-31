3e90d14bcef207919cbbeebe590f5360db748203
Rene Coig <118381189+rcoig@users.noreply.github.com>
2025-01-31 05:18:42 -0800

    Update geometric_mean.R
    - incorporating count into output

--- _source/geometric_mean.R
+++ _source/geometric_mean.R
@@ -1,3 +1,8 @@
 geo <- function(x) 
-{aggr <- ci.mean(x, statistic = "geometric", na.rm = TRUE)
-paste0(round(aggr$geomean, 2), " (", round(aggr$lower, 2), "-", round(aggr$upper,2), ")")}
+{  # Count non-NA values
+  n <- sum(!is.na(x))
+  
+  aggr <- ci.mean(x, statistic = "geometric", na.rm = TRUE)
+  
+  # Include the count in the output
+  paste0("n = ", n, "; ", round(aggr$geomean, 2), " (", round(aggr$lower, 2), "-", round(aggr$upper,2), ")")}
