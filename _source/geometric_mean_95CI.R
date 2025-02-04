9edb34d3e6177fc48a9ce0e32d582c6feeff2cf9
Rene Coig <118381189+rcoig@users.noreply.github.com>
2025-02-03 16:23:08 -0800

    update to geometric mean functions
    -separated into two separate functions, 95% CI vs 90% CI

---
+++ _source/geometric_mean_95CI.R
@@ -0,0 +1,8 @@
+geo95 <- function(x) 
+{  # Count non-NA values
+  n <- sum(!is.na(x))
+  
+  aggr <- ci.mean(x, statistic = "geometric", alpha = 0.05, na.rm = TRUE)
+  
+  # Include the count in the output
+  paste0("n = ", n, "; ", round(aggr$geomean, 2), " (", round(aggr$lower, 2), "-", round(aggr$upper,2), ")")}
